374c8378343ceaa30bbc329238a1cbdded2fb7a0
sen.cenan@gmail.com <sen.cenan@gmail.com>
2016-09-08 20:15:55 -0400

    m: checkout

--- functions/checkout/index.js
+++ functions/checkout/index.js
@@ -1,8 +1,33 @@
 'use strict';
 
+const
+	AWS = require('aws-sdk'),
+	fs = require('fs'),
+	childProcess = require('child_process'),
+
+	ARTI_PATH = './artifects';
+
 exports.handle = function(event, context, callback) {
 
-	console.log(JSON.stringify(event.Records[0].Sns));
-	callback(null, 'abcdedf');
+	const event = JSON.parse(event.Records[0].Sns.Message);
+
+	// create cwd
+	fs.mkdirSync(ARTI_PATH);
+
+	childProcess.exec(
+		"ssh-agent bash -c 'ssh-add ./deployKey; git clone "
+			+ event.repository.ssh_url
+			+ "'",
+		{
+			cwd: ARTI_PATH
+		},
+		(err, stdout, stderr) => {
+			console.log(err);
+			console.log(stdout);
+			console.log(stderr);
+
+			callback(null, '');
+		}
+	);
 
 };
--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,18 @@ const
 	gulp = require('gulp'),
 	prompt = require('gulp-prompt'),
 	transform = require('gulp-transform'),
+	rename = require('gulp-rename'),
 	rm = require('gulp-rimraf'),
 	runSequence = require('gulp-run-sequence'),
 	fs = require('fs');
+
+var cache;
+
+try {
+	cache = require('./cache.json');
+} catch(ex) {
+	cache = {};
+}
 
 gulp.task('clean', function() {
 	return gulp.src('./build/*').pipe(rm());
@@ -25,17 +34,9 @@ gulp.task('copy-project-json', function() {
 });
 
 gulp.task(
-	'transform',
+	'transform-config',
 	['copy-project-json', 'copy-functions'],
 	function() {
-		var cache;
-
-		try {
-			cache = require('./cache.json');
-		} catch(ex) {
-			cache = {};
-		}
-
 		var
 			executorRole = cache.executorRole,
 			mem = require('./project.json').memory;
@@ -47,10 +48,10 @@ gulp.task(
 					{
 						type: 'input',
 						name: 'executorRole',
-						message: 'AWS Role for lambda execution (arn): ',
+						message: 'AWS Role for lambda execution (arn):',
 						default: executorRole
 					},
-					function(result) {
+					(result) => {
 						cache.executorRole = executorRole = result.executorRole;
 					}
 				)
@@ -60,30 +61,57 @@ gulp.task(
 					{
 						type: 'input',
 						name: 'mem',
-						message: 'Sheep C/D executor lambda memory size (Mb): ',
+						message: 'Sheep C/D executor lambda memory size:',
 						default: mem
 					},
-					function(result) {
+					(result) => {
 						mem = result.mem;
 					}
 				)
 			)
 			.pipe(transform(
-				function(contents) {
+				(contents) => {
 					let fnJson = JSON.parse(contents.toString());
 
 					fnJson.role = executorRole;
 					fnJson.memory = mem;
 
-					fs.writeFileSync('./cache.json', JSON.stringify(cache));
-
-					return JSON.stringify(fnJson);
+					return JSON.stringify(fnJson, ' ', 2);
 				}
 			))
-			.pipe(gulp.dest('build'));
+			.pipe(gulp.dest('./build'));
 	}
 );
 
+gulp.task('copy-deploy-key', function() {
+	return gulp
+		.src('./package.json')
+		.pipe(
+			prompt.prompt(
+				{
+					type: 'input',
+					name: 'deployKeyPath',
+					message: 'Path to deploy key:',
+					default: cache.deployKeyPath
+				},
+				(result) => cache.deployKeyPath = result.deployKeyPath
+			)
+		)
+		.pipe(transform(
+			(contents, file) => fs.readFileSync(cache.deployKeyPath)
+		))
+		.pipe(rename('deployKey'))
+		.pipe(gulp.dest('./build/functions/checkout'));
+});
+
+gulp.task('save-config-cache', function() {
+	fs.writeFileSync('./cache.json', JSON.stringify(cache, ' ', 2));
+});
+
 gulp.task('default', function() {
-	runSequence('clean', 'transform');
+	runSequence(
+		'clean',
+		'transform-config', 'copy-deploy-key',
+		'save-config-cache'
+	);
 });
