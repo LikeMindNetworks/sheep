636f72e118cb5afe7bc8a373b34d24527f21542c
sen.cenan@gmail.com <sen.cenan@gmail.com>
2016-09-08 20:33:23 -0400

    m: node432

--- functions/checkout/index.js
+++ functions/checkout/index.js
@@ -3,15 +3,21 @@
 const
 	AWS = require('aws-sdk'),
 	fs = require('fs'),
+	os = require('os'),
+	path = require('path'),
 	childProcess = require('child_process'),
 
-	ARTI_PATH_PREFEX = './sheep-artifects';
+	ARTI_PATH_PREFEX = 'sheep-artifects';
 
 exports.handle = function(event, context, callback) {
 
+	console.log(JSON.stringify(event.Records, ' ', 2));
+
 	const
 		gitEvent = JSON.parse(event.Records[0].Sns.Message),
-		cwd = fs.mkdtempSync(ARTI_PATH_PREFEX);
+		cwd = fs.mkdirSync(path.join(os.tmpdir(), ARTI_PATH_PREFEX));
+
+	console.log(cwd);
 
 	childProcess.exec(
 		"ssh-agent bash -c 'ssh-add ./deployKey; git clone "
