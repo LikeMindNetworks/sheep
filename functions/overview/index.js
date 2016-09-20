26c7d3f313fd8e1312d92f56a4385f75616a97d0
sen.cenan@gmail.com <sen.cenan@gmail.com>
2016-09-20 15:59:02 -0400

    m: render overview

---
+++ functions/overview/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+const
+	AWS = require('aws-sdk'),
+
+	overview = require('./lib/report/overview');
+
+exports.handle = function(event, context, callback) {
+
+	overview.renderHTML(
+		AWS,
+		{
+			stackName: process.env.STACK_NAME
+		}
+	).then(function(html) {
+		callback(null, html)
+	}).catch(function(ex) {
+		callback(ex);
+	});
+
+};
